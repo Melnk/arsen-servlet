538cac257deccfc3167079fa39878d4301b070cb
Melnik <melnikoleg080@gmail.com>
2025-10-03 23:34:05 +0300

    Доработана web часть и начато создание сервлета

---
+++ core/src/main/java/ru/javabegin/micro/demo/FormServlet.java
@@ -0,0 +1,9 @@
+package ru.javabegin.micro.demo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/submit")
+public class FormServlet extends HttpServlet {
+
+}
