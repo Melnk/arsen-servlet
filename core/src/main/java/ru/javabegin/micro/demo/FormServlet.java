164d93971cef4a7c1c634c20bc34044b91f96c16
Melnik <melnikoleg080@gmail.com>
2025-10-04 00:00:17 +0300

    last commit

--- core/src/main/java/ru/javabegin/micro/demo/FormServlet.java
+++ core/src/main/java/ru/javabegin/micro/demo/FormServlet.java
@@ -2,8 +2,34 @@ package ru.javabegin.micro.demo;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 @WebServlet("/submit")
 public class FormServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        if (name == null || email == null
+        || name.trim().length() == 0 || email.trim().length() == 0
+        || !email.contains("@")) {
+            resp.getWriter().println("<!DOCTYPE html><html><body>");
+            resp.getWriter().println("<p style='color:red;'>Ошибка: заполните поля корректно!</p>");
+            resp.getWriter().println("<a href='form.html'>Назад к форме</a>");
+            resp.getWriter().println("</body></html>");
+            return;
+        }
+
+        File file = new File(getServletConte)
+    }
 
 }
